65989a8937ea7a118c8e516c64654da59848c08b
Gireesh <boyina.gireesh@agilysys.com>
2020-12-09 16:27:14 +0530

    - Result screen changes

--- GitHubBranch/ResultViewController.swift
+++ GitHubBranch/ResultViewController.swift
@@ -7,7 +7,14 @@
 //
 
 import Foundation
+import UIKit
+
 class ResultViewController: UIViewController {
     
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("changes in future branch")
+    }
 }
