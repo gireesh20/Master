8ca061dae8ddc69ef5279b4919ba548ecd3aa68f
Gireesh <boyina.gireesh@agilysys.com>
2020-12-09 14:07:45 +0530

    - adding the class ResultViewController

---
+++ GitHubBranch/ResultViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ResultViewController.swift
+//  GitHubBranch
+//
+//  Created by Idcadmin on 18/09/1942 Saka.
+//  Copyright © 1942 Idcadmin. All rights reserved.
+//
+
+import Foundation
+class ResultViewController: UIViewController {
+    
+    
+}
